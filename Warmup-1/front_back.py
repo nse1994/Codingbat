c5311f1ac175f037b59a4fc4a86575c5b676c5c8
Nikhil Edward <nse1@mail.usf.edu>
2020-07-07 10:02:08 -0400

    Add files via upload
    Warmup-1

---
+++ Warmup-1/front_back.py
@@ -0,0 +1,5 @@
+""" Given a string, return a new string where the first and last chars have been exchanged."""
+def front_back(str):
+  if len(str)>1:
+    return(str[len(str)-1] + str[1:len(str)-1] + str[0])
+  return str
